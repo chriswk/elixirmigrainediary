3e02612735a8502833740421e951e6cf0dd80778
chrkolst <christopher.kolstad@finn.no>
2015-11-17 17:16:18 +0100

    Added ribbon

--- web/router.ex
+++ web/router.ex
@@ -7,6 +7,7 @@ defmodule Migrainediary.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug Plug.Ribbon, [:dev, :staging, :test]
   end
 
   pipeline :api do
