1a74753303963e844f78f712db04e78d7f640d9e
Christopher Kolstad <christopher.kolstad@gmail.com>
2015-11-30 18:53:10 +0100

    Travis build failed due to missing database

---
+++ config/travis.exs
@@ -0,0 +1,22 @@
+use Mix.Config
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :migrainediary, Migrainediary.Endpoint,
+  http: [port: 4001],
+  server: false
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
+# Configure your database
+config :migrainediary, Migrainediary.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: "postgres",
+  password: "",
+  database: "migrainediary_test",
+  hostname: "localhost",
+  pool: Ecto.Adapters.SQL.Sandbox
+
+config :comeonin, :bcrypt_log_rounds, 4
+config :comeonin, :pbkdf2_rounds, 1
