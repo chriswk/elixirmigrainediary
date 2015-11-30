03f1f8dd78631e22c9e17b945a77ac31149969df
chrkolst <christopher.kolstad@finn.no>
2015-11-30 17:17:42 +0100

    Try to post device to devices

--- web/static/js/fingerprinting.js
+++ web/static/js/fingerprinting.js
@@ -3,9 +3,19 @@
 export var Fingerprint = {
   run: function run() {
     new Fingerprint2().get((res, comp) => {
-      console.log(res);
-      console.dir(comp);
+      var request = new XMLHttpRequest();
+      var data = {
+        "device": {
+          "device_id": res,
+          "is_mobile": WURFL.is_mobile,
+          "device_name": WURFL.complete_device_name,
+          "form_factor": WURFL.form_factor
+        }
+      };
+      request.open('POST', '/api/devices', true);
+      request.setRequestHeader('Content-Type', 'Content-Type: application/json');
+      request.send(JSON.stringify(data));
     });
     console.log("Hello from fp");
   }
-}
+};
