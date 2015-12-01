a30ffd15acbcdff401e2d5c8abfea8f5e12ee6b6
chrkolst <christopher.kolstad@finn.no>
2015-12-01 20:49:48 +0100

    Remember to change name of app

--- config/travis.exs
+++ config/travis.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
-config :migrainediary, Migrainediary.Endpoint,
+config :mdiary, Migrainediary.Endpoint,
   http: [port: 4001],
   server: false
 
@@ -10,7 +10,7 @@ config :migrainediary, Migrainediary.Endpoint,
 config :logger, level: :warn
 
 # Configure your database
-config :migrainediary, Migrainediary.Repo,
+config :mdiary, Migrainediary.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
   password: "",
